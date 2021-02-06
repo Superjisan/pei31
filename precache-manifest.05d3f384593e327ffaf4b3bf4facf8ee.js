0f514d2499e62c504c4850fc7d863efd9bda6ac3
Jisan Zaman <jzaman@wesleyan.edu>
2021-02-06 18:38:16 -0500

    Updates

---
+++ precache-manifest.05d3f384593e327ffaf4b3bf4facf8ee.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d32e4609153cc9fa71c02e831333a4b3",
+    "url": "/pei31/index.html"
+  },
+  {
+    "revision": "0b63e8a0b17551049044",
+    "url": "/pei31/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "d1e1aa6468008cf08a43",
+    "url": "/pei31/static/js/2.33682824.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pei31/static/js/2.33682824.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b63e8a0b17551049044",
+    "url": "/pei31/static/js/main.f1033e2f.chunk.js"
+  },
+  {
+    "revision": "e9692522fc0a3875e7da",
+    "url": "/pei31/static/js/runtime-main.68d4a396.js"
+  },
+  {
+    "revision": "3ec3c190e6265dd823fc1433aaa75cd9",
+    "url": "/pei31/static/media/birthday-2016.3ec3c190.jpg"
+  },
+  {
+    "revision": "cff18c92674a0553fd54c4b5dbafd1a6",
+    "url": "/pei31/static/media/bridesmaid.cff18c92.png"
+  },
+  {
+    "revision": "4154c51542a1b5d43ee45b56f2cd0df4",
+    "url": "/pei31/static/media/camping.4154c515.jpg"
+  },
+  {
+    "revision": "a0ce9e13522b16717de7c3a1f601a611",
+    "url": "/pei31/static/media/christmas-2017.a0ce9e13.jpg"
+  },
+  {
+    "revision": "0b247b5966855a0dcfabf16da93ac2ec",
+    "url": "/pei31/static/media/classy-af.0b247b59.jpg"
+  },
+  {
+    "revision": "2ed3cfb688cf19d57d1884274e00aae9",
+    "url": "/pei31/static/media/corkscrew.2ed3cfb6.jpg"
+  },
+  {
+    "revision": "479a529c7593a87a17cabafe0a06f15f",
+    "url": "/pei31/static/media/cuba-gangsta.479a529c.png"
+  },
+  {
+    "revision": "145a578cedf2b5da79777982f24cc3e5",
+    "url": "/pei31/static/media/glasses.145a578c.png"
+  },
+  {
+    "revision": "fd7b2cd4432a2fd7ac771f5e210b2fda",
+    "url": "/pei31/static/media/gold-toilet.fd7b2cd4.png"
+  },
+  {
+    "revision": "3ffa0c7a81c334ed47b2afd716406ce0",
+    "url": "/pei31/static/media/hard-at-work.3ffa0c7a.jpg"
+  },
+  {
+    "revision": "0fadb32aea0c36c3cbf458ce743233ab",
+    "url": "/pei31/static/media/hotness.0fadb32a.jpg"
+  },
+  {
+    "revision": "57e943e9d05cd5009e069e2a7824227f",
+    "url": "/pei31/static/media/japanese-dress.57e943e9.png"
+  },
+  {
+    "revision": "a944fa11afbb2aa526a03e33297d7978",
+    "url": "/pei31/static/media/laurapose.a944fa11.jpg"
+  },
+  {
+    "revision": "c18bcfa3b372778774a2b18a8b3f70ae",
+    "url": "/pei31/static/media/maddie.c18bcfa3.png"
+  },
+  {
+    "revision": "e70ab8b16e435b25c790eead26b4bd99",
+    "url": "/pei31/static/media/noodles.e70ab8b1.png"
+  },
+  {
+    "revision": "fc226691329542d138a208285efb4201",
+    "url": "/pei31/static/media/not-impressed.fc226691.jpg"
+  },
+  {
+    "revision": "f1c384e8a2c85d67887d145de215f0f4",
+    "url": "/pei31/static/media/not-weird.f1c384e8.jpg"
+  },
+  {
+    "revision": "af9d5be2e8cfe5d92add94af2f2c6131",
+    "url": "/pei31/static/media/peace.af9d5be2.jpg"
+  },
+  {
+    "revision": "c71bfec5ffe626ec98ffd44acb4081b9",
+    "url": "/pei31/static/media/peppers.c71bfec5.png"
+  },
+  {
+    "revision": "cf2c8a8bf1d5f3efa94411e510629862",
+    "url": "/pei31/static/media/pigeondance.cf2c8a8b.jpg"
+  },
+  {
+    "revision": "6e53249358d2071e47996532e1a93d65",
+    "url": "/pei31/static/media/poison-ivy.6e532493.jpg"
+  },
+  {
+    "revision": "6e41ea5ebe54fa4d48a7092327759307",
+    "url": "/pei31/static/media/prom.6e41ea5e.jpg"
+  },
+  {
+    "revision": "467de972ee6448a44ce244cd34aabae7",
+    "url": "/pei31/static/media/pure-joy.467de972.jpg"
+  },
+  {
+    "revision": "8fb82ec5a0ea8c20d6d3a78a4faa3221",
+    "url": "/pei31/static/media/rock-elegant.8fb82ec5.jpg"
+  },
+  {
+    "revision": "f1e6150aeea5e6a5cf49d26e8a9d7c43",
+    "url": "/pei31/static/media/scheming.f1e6150a.jpg"
+  },
+  {
+    "revision": "017e4628da586deb556a531e0b3c8a73",
+    "url": "/pei31/static/media/shakespeare.017e4628.jpg"
+  },
+  {
+    "revision": "434d4e6adec9d1c69a27e4d88fbd94fe",
+    "url": "/pei31/static/media/sister-pose.434d4e6a.jpg"
+  },
+  {
+    "revision": "e8f7517f5d30112d5ed942b880493c31",
+    "url": "/pei31/static/media/swimming.e8f7517f.png"
+  },
+  {
+    "revision": "677646cd6dd1bee95648cb4bc8a237c9",
+    "url": "/pei31/static/media/victory.677646cd.jpg"
+  },
+  {
+    "revision": "74416c25eb8ae3e8a2676644d6f08851",
+    "url": "/pei31/static/media/wings.74416c25.jpg"
+  }
+]);
